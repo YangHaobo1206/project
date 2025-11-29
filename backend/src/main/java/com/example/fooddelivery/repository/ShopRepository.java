e844e4b6debd9c238e238f7f99b4250603fa51a4
YangHaobo1206 <1052612303@qq.com>
2025-11-30 03:50:45 +0800

    Delete backend directory

--- backend/src/main/java/com/example/fooddelivery/repository/ShopRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.fooddelivery.repository;
-
-import com.example.fooddelivery.entity.Shop;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShopRepository extends JpaRepository<Shop, Long> {
-}
